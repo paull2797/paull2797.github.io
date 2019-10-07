804d07e777ef4034fee602d54efcfe4665490bf2
paull2797 <43858674+paull2797@users.noreply.github.com>
2019-10-07 09:37:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Paul Liu"
+let userName = "paull2797";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
